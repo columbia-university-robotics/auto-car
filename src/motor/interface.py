0b064be4d69b899f217fefbb0f53d6becffa076f
Anthony Krivonos <abkrivonos@gmail.com>
2019-11-20 23:01:54 -0500

    Fixed motor interface bug

--- src/motor/interface.py
+++ src/motor/interface.py
@@ -3,28 +3,32 @@
 # All Rights Reserved. Development by the Autonomous Car Team.
 # Contact: Anthony, anthony.k@columbia.edu
 
-from enum import Enum
+import sys
 import atexit
 import rospy
 import numpy as np
 import time
+
+from enum import Enum
+from os.path import join, dirname
+
+sys.path.append(join(dirname(__file__), '../..'))
+from src.util.logger import Logger
+
 from std_msgs.msg import Int16MultiArray
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
-from src.util.logger import Logger
 
 class Interface:
-
     """
     Abstract:
     Simple script to baseline motor execution.
     """
 
-    def __init__(self, speed = 1):
+    def __init__(self):
         self.LOGGER = Logger(self)
         self.controller = Adafruit_MotorHAT(addr=0x60, i2c_bus=1)
         self.motors = [0, 0, 0, 0]
-        self.speed = speed
         self.publisher = rospy.Publisher('/motor/pub', Int16MultiArray, queue_size=1, latch=True)
         # Initialize motor subscribers
         rospy.init_node('interface')
@@ -36,18 +40,6 @@ class Interface:
         r = rospy.Rate(2)
         while not rospy.is_shutdown():
             self.LOGGER.info(str(self.motors))
-            # if self.motors[0] is self.motors[2] and self.motors[1] is self.motors[3]:
-            #     self.send([0, 0, 0, 0])
-            # else:
-            #     am = np.argmax(self.motors)
-            #     self.LOGGER.info("AM " + am)
-            #     if am == 0:
-            #         self.send([self.speed, self.speed, self.speed, self.speed])
-            #     elif am == 1:
-            #         self.send([-self.speed, self.speed, -self.speed, self.speed])
-            #     else:
-            #         assert am == 2
-            #         self.send([self.speed, -self.speed, self.speed, -self.speed])
             r.sleep()
 
     def turn_off_motors(self):
@@ -74,12 +66,22 @@ class Interface:
         self.motors = values
 
         for idx, motor_speed in enumerate(values):
-            direction = Adafruit_MotorHAT.FORWARD if motor_speed > 0 else Adafruit_MotorHAT.BACKWARD if motor_speed < 0 else Adafruit_MotorHAT.BRAKE
+            if motor_speed > 0:
+                direction = Adafruit_MotorHAT.FORWARD
+            elif motor_speed < 0:
+                direction = Adafruit_MotorHAT.BACKWARD
+            else:
+                direction = Adafruit_MotorHAT.BRAKE
+            mh.getMotor.setSpeed(abs(motor_speed * 255))
             mh.getMotor(idx + 1).run(direction)
-            myMotor.setSpeed(abs(motor_speed * 255))
+
 
 class Wheel(Enum):
     FRONT_LEFT = 0
     FRONT_RIGHT = 1
     BACK_LEFT = 2
     BACK_RIGHT = 3
+
+
+if __name__ is '__main__':
+    Interface()
