66a029adcf00ce707a9631398bb7f09d6ea6481b
Autobot <curccar@gmail.com>
2019-11-23 15:53:43 -0500

    IT WORKS (ROS CLI based movement)

--- src/motor/interface.py
+++ src/motor/interface.py
@@ -3,15 +3,22 @@
 # All Rights Reserved. Development by the Autonomous Car Team.
 # Contact: Anthony, anthony.k@columbia.edu
 
+import os
+import sys
 from enum import Enum
 import atexit
 import rospy
 import numpy as np
 import time
-from std_msgs.msg import Int16MultiArray
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import threading
+from os.path import join, dirname
+from std_msgs.msg import String
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(join(dirname(__file__), '../..'))
 from src.util.logger import Logger
+import Adafruit_GPIO.I2C as I2C
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 class Interface:
 
@@ -20,15 +27,13 @@ class Interface:
     Simple script to baseline motor execution.
     """
 
-    def __init__(self, speed = 1):
+    def __init__(self):
         self.LOGGER = Logger(self)
         self.controller = Adafruit_MotorHAT(addr=0x60, i2c_bus=1)
         self.motors = [0, 0, 0, 0]
-        self.speed = speed
-        self.publisher = rospy.Publisher('/motor/pub', Int16MultiArray, queue_size=1, latch=True)
         # Initialize motor subscribers
         rospy.init_node('interface')
-        rospy.Subscriber("/motor", Int16MultiArray, self.on_motor_callback)
+        rospy.Subscriber("/motor", String, self.on_motor_callback, queue_size=1)
         # Turn off motors when the script exits.
         atexit.register(self.turn_off_motors)
 
@@ -55,31 +60,39 @@ class Interface:
         Turns off all motors.
         """
         self.LOGGER.info("Stopping motors...")
-        self.send([0, 0, 0, 0])
+        self.turn_motors([0, 0, 0, 0])
 
-    def send(self, values):
-        """
-        Performs motor movement through the publisher.
-        """
-        self.LOGGER.info("Publish [%s]" % values)
-        msg = Int16MultiArray()
-        msg.data = values
-        self.publisher.publish(msg)
+    def turn_motors(self, values):
+	"""
+	ACTUALLY turns the motors.
+	"""
 
-    def on_motor_callback(self, values):
+	if self.motors == values or len(values) is not 4:
+	    return
+
+	self.LOGGER.info("Motors turning: " + str(values))
+
+	while self.motors[0] != values[0] and self.motors[1] != values[1] and self.motors[2] != values[2] and self.motors[3] != values[3]:
+	    for idx, motor_speed in enumerate(values):
+                direction = Adafruit_MotorHAT.FORWARD if self.motors[idx] > 0 else Adafruit_MotorHAT.BACKWARD if self.motors[idx] < 0 else Adafruit_MotorHAT.BRAKE
+                self.controller.getMotor(idx + 1).run(direction)
+                self.motors[idx] += (1 if self.motors[idx] < values[idx] else -1 if self.motors[idx] > values[idx] else 0)
+                self.controller.getMotor(idx + 1).setSpeed(int(abs(self.motors[idx])))
+            time.sleep(0.001)
+
+    def on_motor_callback(self, value_str_obj):
         """
         Performs the actual motor operations.
         """
-        assert len(values) is 4
-        self.motors = values
 
-        for idx, motor_speed in enumerate(values):
-            direction = Adafruit_MotorHAT.FORWARD if motor_speed > 0 else Adafruit_MotorHAT.BACKWARD if motor_speed < 0 else Adafruit_MotorHAT.BRAKE
-            mh.getMotor(idx + 1).run(direction)
-            myMotor.setSpeed(abs(motor_speed * 255))
+	values = list(map(lambda x: float(x), value_str_obj.data.replace("\\", "").replace(" ", "").split(",")))
+	self.turn_motors(values)
+        self.motors = values
 
 class Wheel(Enum):
     FRONT_LEFT = 0
     FRONT_RIGHT = 1
     BACK_LEFT = 2
     BACK_RIGHT = 3
+
+Interface()
